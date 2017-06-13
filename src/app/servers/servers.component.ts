acc65615332d1b862b01399d996c0f452b1d2c8f
Waagna <dominik.waagna@gmail.com>
2017-06-13 21:16:23 +0200

    Advancing on course

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
+  serverCreated = false;
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
